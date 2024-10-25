ecb1d855242e76484ec7a55f1b68adcb2fbe2ce1
Benjamin Bustamante <benjamin.bustamante@usach.cl>
2024-10-25 11:56:32 -0300

    Update ClientController.java

--- backendBanco/src/main/java/com/example/backendBanco/controllers/ClientController.java
+++ backendBanco/src/main/java/com/example/backendBanco/controllers/ClientController.java
@@ -7,5 +7,42 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+@RestController
+@RequestMapping("/api/v1/clients")
+@CrossOrigin("*")
+
 public class ClientController {
+    @Autowired
+    ClientService clientService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<ClientEntity>> listClients() {
+        List<ClientEntity> clients = clientService.getClients();
+        return ResponseEntity.ok(clients);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientEntity> getClientById(@PathVariable Long id) {
+        ClientEntity client = clientService.getClientById(id);
+        return ResponseEntity.ok(client);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<ClientEntity> saveClient(@RequestBody ClientEntity client) {
+        ClientEntity clientNew = clientService.saveClient(client);
+        return ResponseEntity.ok(clientNew);
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<ClientEntity> updateClient(@RequestBody ClientEntity client){
+        ClientEntity clientUpdated = clientService.updateClient(client);
+        return ResponseEntity.ok(clientUpdated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deleteClientById(@PathVariable Long id) throws Exception {
+        var isDeleted = clientService.deleteClient(id);
+        return ResponseEntity.noContent().build();
+    }
 }
