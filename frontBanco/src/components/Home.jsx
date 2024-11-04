e64b330f960ab248ccad538848407dca02bd5a50
Benjamin Bustamante <benjamin.bustamante@usach.cl>
2024-11-03 21:18:01 -0300

    Changes

--- frontBanco/src/components/Home.jsx
+++ frontBanco/src/components/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1>Prestabanco: Sistema de gestión crédito</h1>
+      <h1>Prestabanco: Sistema de gestión crédito para todas/os</h1>
       <p>
       </p>
     </div>
